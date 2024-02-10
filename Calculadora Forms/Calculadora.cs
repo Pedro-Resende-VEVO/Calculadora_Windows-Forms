c469c78435dd28fa6ce8618785b3533f4aa6beca
Pedro Henrique Resende Menezes <pedrohrm050315@hotmail.com>
2024-02-10 10:40:42 -0300

    VerCompleta01

--- Calculadora Forms/Calculadora.cs
+++ Calculadora Forms/Calculadora.cs
@@ -12,7 +12,7 @@ namespace Calculadora_Forms
 {
     public partial class Calculadora : Form
     {
-        bool ordem = true;
+        bool cond;
 
         public double valor1;
 
@@ -31,10 +31,58 @@ namespace Calculadora_Forms
 
         }
 
+        //Números
+        private void bd0_Click(object sender, EventArgs e)
+        {
+            tbDisplay.Text += "0";
+        }
+
         private void bd1_Click(object sender, EventArgs e)
         {
             tbDisplay.Text += "1";
         }
+
+        private void bd2_Click(object sender, EventArgs e)
+        {
+            tbDisplay.Text += "2";
+        }
+
+        private void bd3_Click(object sender, EventArgs e)
+        {
+            tbDisplay.Text += "3";
+        }
+
+        private void bd4_Click(object sender, EventArgs e)
+        {
+            tbDisplay.Text += "4";
+        }
+
+        private void bd5_Click(object sender, EventArgs e)
+        {
+            tbDisplay.Text += "5";
+        }
+
+        private void bd6_Click(object sender, EventArgs e)
+        {
+            tbDisplay.Text += "6";
+        }
+
+        private void bd7_Click(object sender, EventArgs e)
+        {
+            tbDisplay.Text += "7";
+        }
+
+        private void bd8_Click(object sender, EventArgs e)
+        {
+            tbDisplay.Text += "8";
+        }
+
+        private void bd9_Click(object sender, EventArgs e)
+        {
+            tbDisplay.Text += "9";
+        }
+
+        //Funções das operações
 
         private void btMais_Click(object sender, EventArgs e)
         {
@@ -53,6 +101,128 @@ namespace Calculadora_Forms
                 btIgual_Click(sender, e);
             }
         }
+
+        private void btMenos_Click(object sender, EventArgs e)
+        {
+            Calculos obj = new Calculos();
+
+            operacao = 2;
+
+            if (valor1 == 0)
+            {
+                tbOperacao.Text = tbDisplay.Text + " -";
+                valor1 = double.Parse(tbDisplay.Text);
+                tbDisplay.Text = "";
+            }
+            else
+            {
+                btIgual_Click(sender, e);
+            }
+        }
+
+        private void btVezes_Click(object sender, EventArgs e)
+        {
+            Calculos obj = new Calculos();
+
+            operacao = 3;
+
+            if (valor1 == 0)
+            {
+                tbOperacao.Text = tbDisplay.Text + " x";
+                valor1 = double.Parse(tbDisplay.Text);
+                tbDisplay.Text = "";
+            }
+            else
+            {
+                btIgual_Click(sender, e);
+            }
+        }
+
+        private void btDividir_Click(object sender, EventArgs e)
+        {
+            Calculos obj = new Calculos();
+
+            operacao = 4;
+
+            if (valor1 == 0)
+            {
+                tbOperacao.Text = tbDisplay.Text + " /";
+                valor1 = double.Parse(tbDisplay.Text);
+                tbDisplay.Text = "";
+            }
+            else
+            {
+                btIgual_Click(sender, e);
+            }
+        }
+
+        //Funções auxiliares
+
+        private void btReset_Click(object sender, EventArgs e)
+        {
+            tbDisplay.Text = "";
+            tbOperacao.Text = "";
+            valor1 = 0;
+            valor2 = 0;
+        }
+
+        private void btLimpar_Click(object sender, EventArgs e)
+        {
+            tbDisplay.Text = "";
+        }
+
+        private void btApagar_Click(object sender, EventArgs e)
+        {
+            string displayCompl = "";
+
+            for (int i = 0; i < tbDisplay.Text.Length - 1; i++)
+            {
+                displayCompl += tbDisplay.Text[i];
+            }
+
+            tbDisplay.Text = displayCompl;
+        }
+
+        private void btSinal_Click(object sender, EventArgs e)
+        {
+            if (tbDisplay.Text[0] == '-')
+            {
+                string displayCompl = "";
+
+                for (int i = 1; i < tbDisplay.Text.Length; i++)
+                {
+                    displayCompl += tbDisplay.Text[i];
+                }
+
+                tbDisplay.Text = displayCompl;
+            }
+            else
+            {
+                string modificado = tbDisplay.Text.Insert(0, "-");
+                tbDisplay.Text = modificado;
+            }
+        }
+
+        private void btPonto_Click(object sender, EventArgs e)
+        {
+            cond = false;
+
+            for (int i = 0; i < tbDisplay.Text.Length; i++)
+            {
+                if (tbDisplay.Text[i] == ',')
+                {
+                    cond = true;
+                    MessageBox.Show("Insira apenas uma vírgula");
+                }
+            }
+
+            if (cond == false)
+            {
+                tbDisplay.Text += ",";
+            }
+        }
+
+        //Função igual
 
         private void btIgual_Click(object sender, EventArgs e)
         {
@@ -98,6 +268,7 @@ namespace Calculadora_Forms
                 tbOperacao.Text = "";
             }
         }
+
     }
 
     class Calculos
